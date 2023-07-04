8afbabcab3d58f552e6c49e75f8e054e9b3ec32c
Sreekanth V <25sreekanth@gmail.com>
2023-07-04 15:47:52 -0400

    style: header and login page

--- e_learn/views.py
+++ e_learn/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import User, Institution, Course, UserCourseRelation, Assignment, AssignmentGrades, UserInstitutionRelation, Subscription
+import sweetify
 
 def home(request):
     """ Dashboard of the application """
