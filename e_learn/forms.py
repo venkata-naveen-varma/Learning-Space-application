fb030f312ab10537aa1b35afd811f5e33c4d4218
aiswarya-siby <aiswaryasiby11@gmail.com>
2023-07-26 23:03:52 -0400

    style: background images added.

--- e_learn/forms.py
+++ e_learn/forms.py
@@ -4,6 +4,12 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(AuthenticationForm):
     """ Login form to allow user to enter credentials """
+    username = forms.CharField(
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'}))
+    password = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'}))
     class Meta:
         model = User
         fields = ('username', 'password')
